ee3f131967070288e88a352849a22590c17e7ee1
Amin Abbasloo <abbasloo@cs.uni-bonn.de>
2019-01-23 01:22:41 +0100

    Delete delete.sh

--- Segmentation/delete.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-rm -R ../data/pc/*.pcd
-rm -R ../data/pc/done/*.pcd
-rm -R ../data/pc_simp/*.pcd
-rm -R ../data/mesh/*.obj
-rm -R ../data/mesh/*.vtk
-rm -R ../data/mesh_simp/*.obj
-rm -R ../data/mesh_simp/*.vtk
